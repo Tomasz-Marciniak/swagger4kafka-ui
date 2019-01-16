91237e9dee82d34a4cdca83704ca3237e3849426
Stav Shamir <stav.shamir@ecitele.com>
2019-01-16 14:18:33 +0200

    Implement topic component

--- src/app/topics/topic/topic.component.ts
+++ src/app/topics/topic/topic.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {KafkaEndpoint} from '../kafka-endpoint.model';
 
 @Component({
   selector: 'app-topic',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topic.component.css']
 })
 export class TopicComponent implements OnInit {
+  @Input() private endpoint: KafkaEndpoint;
+  private defaultExampleValue: string;
+  private exampleLineCount: number;
+  private isOpen = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.defaultExampleValue = JSON.stringify(this.endpoint.payloadExample, null, 2);
+    this.exampleLineCount = 1 + this.defaultExampleValue.split('\n').length;
   }
 
 }
